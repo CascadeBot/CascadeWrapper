ec223c3bd6a1bc5a28cccc6913b1dab5e7b35141
William Oldham <wegg7250@gmail.com>
2018-11-12 21:28:06 +0000

    All dem operations

--- src/main/java/com/cascadebot/cascadewrapper/Operation.java
+++ src/main/java/com/cascadebot/cascadewrapper/Operation.java
@@ -1,13 +1,12 @@
 package com.cascadebot.cascadewrapper;
 
-import com.cascadebot.shared.ExitCodes;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public enum Operation {
 
     NOOP,
+    START,
+    STOP,
+    RESTART,
+    UPDATE,
     FORCE_STOP, // Force operations should never be able be called from the bot
     FORCE_RESTART,
     FORCE_UPDATE,
